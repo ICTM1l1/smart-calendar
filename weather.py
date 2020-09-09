b7486f87aef5118b8e55533b30a549e64a872013
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-09-09 15:17:56 +0200

    add automatic historical data handling, better returns

--- weather.py
+++ weather.py
@@ -2,11 +2,13 @@ import json
 import urllib
 import urllib3
 import ipinfo
+import datetime
 import read_keys
 
 endpoint = "api.weatherapi.com/v1/forecast.json?"
-http = urllib3.PoolManager()
+history = "api.weatherapi.com/v1/history.json?"
 
+http = urllib3.PoolManager()
 keys = read_keys.keys()
 
 def get_ip_city():
@@ -14,10 +16,36 @@ def get_ip_city():
 	details = handler.getDetails()
 	return details.city
 
-def request_forecast(key, location, days):
-	params = urllib.parse.urlencode({'key': key, 'q': location, 'days': days})
-	url = endpoint + params
-	data = http.request("GET", url)
-	return json.loads(data.data.decode("UTF-8"))
+def historic_p(d):
+	now = datetime.date.today()
+	if type(d) == datetime.datetime:
+		return d.date() < now
+	else:
+		past = datetime.datetime.strptime(d, "%Y-%m-%d")
+		return past.date() < now
 
-forecast = request_forecast(keys.weatherapi, get_ip_city(), 7)
+def handle_forecast(f):
+	if "error" in f.keys():
+		if "30 days only" in f["error"]["message"]:
+			return None
+		else:
+			return f["error"]["message"]
+	else:
+		return f['forecast']['forecastday'][0]['day']
+
+# date is either a datetime object or a date string formatted as
+# YYYY-mm-dd. it automatically detects if it historic data or not
+# and returns it as such. some fields are missing when it is
+# historic data, such as `condition` or `daily_chance_of_rain`
+def request_forecast(key, location, date):
+	params = urllib.parse.urlencode({'key': key, 'q': location, 'dt': date})
+	url = None
+	if historic_p(date):
+		url = history + params
+	else:
+		url = endpoint + params
+	print(url)
+	data = http.request("GET", url)
+	return handle_forecast(json.loads(data.data.decode("UTF-8")))
+
+#forecast = request_forecast(keys.weatherapi, get_ip_city(), '2020-09-12')
