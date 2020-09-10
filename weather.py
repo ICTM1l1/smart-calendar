2f32a973fed80cc15fec09770ee556892cc3a6ec
Koen van Meijeren <koenvanmeijeren@gmail.com>
2020-09-10 13:26:30 +0200

    Integrate weather api in main program

--- weather.py
+++ weather.py
@@ -12,38 +12,38 @@ http = urllib3.PoolManager()
 keys = read_keys.keys()
 
 def get_ip_city():
-	handler = ipinfo.getHandler()
-	details = handler.getDetails()
-	return details.city
+    handler = ipinfo.getHandler()
+    details = handler.getDetails()
+    return details.city
 
 def historic_p(d):
-	now = datetime.date.today()
-	if type(d) == datetime.datetime:
-		return d.date() < now
-	else:
-		past = datetime.datetime.strptime(d, "%Y-%m-%d")
-		return past.date() < now
+    now = datetime.date.today()
+    if type(d) == datetime.datetime:
+        return d.date() < now
+    else:
+        past = datetime.datetime.strptime(d, "%Y-%m-%d")
+        return past.date() < now
 
 def handle_forecast(f):
-	if "error" in f.keys():
-		if "30 days only" in f["error"]["message"]:
-			return None
-		else:
-			return f["error"]["message"]
-	else:
-		return f['forecast']['forecastday'][0]['day']
+    if "error" in f.keys():
+        if "30 days only" in f["error"]["message"]:
+            return None
+        else:
+            return f["error"]["message"]
+    else:
+        return f['forecast']['forecastday'][0]['day']
 
 # date is either a datetime object or a date string formatted as
 # YYYY-mm-dd. it automatically detects if it historic data or not
 # and returns it as such. some fields are missing when it is
 # historic data, such as `condition` or `daily_chance_of_rain`
 def request_forecast(date, key = keys.weatherapi, location = get_ip_city()):
-	params = urllib.parse.urlencode({'key': key, 'q': location, 'dt': date})
-	url = None
-	if historic_p(date):
-		url = history + params
-	else:
-		url = endpoint + params
-	print(url)
-	data = http.request("GET", url)
-	return handle_forecast(json.loads(data.data.decode("UTF-8")))
+    params = urllib.parse.urlencode({'key': key, 'q': location, 'dt': date})
+    url = None
+    if historic_p(date):
+        url = history + params
+    else:
+        url = endpoint + params
+
+    data = http.request("GET", url)
+    return handle_forecast(json.loads(data.data.decode("UTF-8")))
